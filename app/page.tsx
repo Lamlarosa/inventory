ccbc5690e14786831b343328f9801d89ec3b6bae
Lamlarosa <alamlars17@gmail.com>
2025-01-17 07:09:01 +0700

    lok

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+import { buttonVariants } from "@/components/ui/button";
+import { PageRoutes } from "@/lib/pageroutes";
+
+export default function Home() {
+  return (
+    <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
+      <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
+        Documents techmart
+      </h1>
+      <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
+      Techmart adalah aplikasi belanja online khusus untuk produk elektronik yang dirancang untuk memberikan pengalaman belanja yang mudah, aman, dan nyaman bagi pengguna.
+      </p>
+      <div className="flex items-center gap-5">
+        <Link
+          href={`/docs${PageRoutes[0].href}`}
+          className={buttonVariants({ className: "px-6", size: "lg" })}
+        >
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ settings/documents.ts
@@ -0,0 +1,186 @@
+import { Paths } from "@/lib/pageroutes";
+
+export const Documents: Paths[] = [
+  {
+    title: "Pendahuluan",
+    href: "/pendahuluan",
+    heading: "Getting started",
+    items: [
+      {
+        title: "Teknologi Yang Digunakan",
+        href: "/teknologi-yang-digunakan",
+      },
+      {
+        title: "Keterkaitan Aplikasi Inventory dengan Mata Kuliah",
+        href: "/Kaitan Matkul",
+      },
+    ],
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Navigation",
+    href: "/navigation",
+    heading: "Documents",
+  },
+  {
+    title: "Analisis dan desain perangkat lunak",
+    href: "/structure",
+    items: [
+      {
+        title: "Analisis Kebutuhan",
+        href: "/deeper",
+        items : [
+          {
+            title: "Business Requirement",
+            href: "/business-requirement",
+          },
+          {
+            title: "Stakeholder Requirement",
+            href: "/stakeholder-requirement",
+          },
+          {
+            title: "System Requirement",
+            href: "/sistem-requirement",
+          },
+          {
+            title: "Software Requirement",
+            href: "/software-requirement",
+          },
+        ],
+      },
+      {
+        title: "Diagram",
+        href: "/Diagram",
+        items : [
+          {
+            title: "Use Case Diagram",
+            href: "/Use-Case-Diagram",
+          },
+          {
+            title: "Diagram Class",
+            href: "/Diagram-Class",
+          },
+          {
+            title: "Flow Chart",
+            href: "/FlowChart",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Perancangan Desain Antarmuka",
+    href: "/perancangan-desain-antarmuka",
+    items: [
+      {
+        title: "Dokumen Desain",
+        href: "/dokumen-desain",
+        items: [
+          {
+            title: "Sketsa Awal",
+            href: "/sketsa-awal",
+          },
+          {
+            title: "Wireframe",
+            href: "/wireframe",
+          },
+          {
+            title: "Userflow",
+            href: "/userflow",
+          },
+          {
+            title: "prototipe",
+            href: "/prototype",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Pengembangan Aplikasi",
+    href: "/pengembangan-aplikasi",
+    items: [
+      {
+        title: "Metode LDLC",
+        href: "/metode-LDLC",
+        items: [
+          {
+            title: "Analisis Kebutuhan",
+            href: "/analisis-kebutuhan",
+          },
+          {
+            title: "Pembuatan Diagram UML",
+            href: "/pembuatan-diagram-UML",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Dokumentasi Penggunaan Website",
+    href: "/dokumentasi-penggunaan-website",
+    items: [
+      {
+        title: "Panduan Penggunaan Fitur Utama Dan Alur Sistem",
+        href: "/panduan-penggunaan-fitur",
+      },
+    ],
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Markup",
+    href: "/markup",
+    heading: "Components",
+    items: [
+      {
+        title: "Cards",
+        href: "/cards",
+      },
+      {
+        title: "Diagrams",
+        href: "/diagrams",
+      },
+      {
+        title: "Desain",
+        href: "/filetree",
+      },
+      {
+        title: "Lists",
+        href: "/lists",
+      },
+      {
+        title: "Maths",
+        href: "/maths",
+      },
+      {
+        title: "Notes",
+        href: "/notes",
+      },
+      {
+        title: "Steps",
+        href: "/steps",
+      },
+      {
+        title: "Table",
+        href: "/table",
+      },
+      {
+        title: "Tabs",
+        href: "/tabs",
+      },
+    ],
+  },
+];
